bf41d88a36066c56b8163864d7472b28792981f8
Luan Almeida <luanbianchine1@gmail.com>
2022-07-09 11:24:56 -0300

    splash screen completed

--- App.tsx
+++ App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {SignIn} from './src/screens/signIn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SignIn/>    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/signIn/index.tsx
@@ -0,0 +1,19 @@
+import {useState} from 'react'
+import {View, Text, TextInput} from 'react-native';
+import {styles} from './styles';
+
+export function SignIn(){
+    const [text, setText] = useState('')
+    return(
+        <View style={styles.container}>
+            <Text>Sign In</Text>
+            <TextInput 
+                style={styles.input}
+                onChangeText={setText}
+            />
+            <Text>
+                Você digitou: {text} 
+            </Text>
+        </View>
+    )
+}
